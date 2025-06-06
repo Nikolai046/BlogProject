5d8b3120d3dba97ba378beecd6d7230d1c84f5de
NA <nakursk@gmail.com>
2025-06-06 17:32:40 +0300

    Начал работать над методом "Создать статью"_2

--- BlogProject.Core/Models/ViewModels/ArticleViewModel.cs
+++ BlogProject.Core/Models/ViewModels/ArticleViewModel.cs
@@ -1,9 +1,19 @@
-﻿namespace BlogProject.Core.Models.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogProject.Core.Models.ViewModels;
 
 public class ArticleViewModel
 {
     public int ArticleId { get; set; }
-    public required string Title { get; set; }
+
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "Поле {0} должно иметь минимум {2} и максимум {1} символов.")]
+    [Required(ErrorMessage = "Заполните это поле")]
+    [Display(Name = "Заголовок")]
+    public string Title { get; set; } = string.Empty;
+
+    [StringLength(10000, MinimumLength = 2, ErrorMessage = "Поле {0} должно иметь минимум {2} и максимум {1} символов.")]
+    [Required(ErrorMessage = "Заполните это поле")]
+    [Display(Name = "Содержагие статьи")]
     public required string Content { get; set; }
     public string? AuthorFullName { get; set; }
     public DateTime CreatedDate { get; set; }
---
+++ BlogProject.Core/Models/ViewModels/EditArticleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogProject.Core.Models.ViewModels;
+
+public class EditArticleViewModel
+{
+    [Required(ErrorMessage = "Заполните это поле")]
+    [Display(Name = "Заголовок")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Заполните это поле")]
+    [Display(Name = "Содержание")]
+    public string Content { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? UpDateTime { get; set; }
+    public int? articleId { get; set; }
+
+    public string? Tags { get; set; }
+}
